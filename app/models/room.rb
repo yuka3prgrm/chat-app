14354408cdae47f7de00249be79c2f20877c61d7
yuka3prgrm <yuka3prgrm@gmail.com>
2023-05-29 15:52:10 +0900

    Revert "Merge pull request #2 from yuka3prgrm/チャットルーム管理機能の実装"
    This reverts commit 827b65418d613e9dba831ec9031245f393e927b9, reversing
    changes made to 8fb50ae3fac51bbb4bce0a132cd2d7eaedb8bdc2.

--- app/models/room.rb
+++
@@ -1,7 +0,0 @@
-class Room < ApplicationRecord
-  has_many :room_users
-  has_many :users, through: :room_users
-
-  validates :name, presence: true
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "rooms#index"
+  get 'messages/index'
+  root to: "messages#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
 end
